990029e922f2dd6ba04b84a571ee489896c4a736
PSHamke <hamkelektrik@gmail.com>
2022-04-01 15:02:14 +0300

    class 4/1

---
+++ Classes/Practice/4.1.2022/script.js
@@ -0,0 +1,21 @@
+console.log("Hello world");
+//alert("Alert!");
+
+let a = 5+4+"6"*5/5
+a = 6+"6"// handle it as a string
+b= "This is my sentence"
+c= b.split(" "); // parse WS as a delimiter
+console.log(a);
+console.log(c[0]); // first word of sentence
+console.log(b.toLowerCase()); // lowered 
+
+let types = ["Electric", "Water", true]; // Python like list
+console.log(types);
+
+myFunc();
+
+function myFunc(){
+    b= "Function called !"
+    console.log(b.split(" "));
+}
+
